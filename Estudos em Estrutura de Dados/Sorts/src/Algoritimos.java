fff2f323a2c1a3c9addea6f06640d1d35b8f69ab
André <andrefm06@gmail.com>
2026-07-02 12:51:49 -0300

    Add Bubble Sort algorithm.

--- Estudos em Estrutura de Dados/Sorts/src/Algoritimos.java
+++ Estudos em Estrutura de Dados/Sorts/src/Algoritimos.java
@@ -1,4 +1,17 @@
 
 public class Algoritimos {
+    private int[] array = new int[20];
 
+    public void buubleSort(){
+        int size = array.length;
+        for(int i = 0; i < size-1; i++){
+            for(int j = 0; j < size - 1 - i;j++){
+                if(array[j]>array[j+1]){
+                    int aux = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = aux;
+                }
+            }
+        }
+    }
 }
