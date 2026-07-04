c14d13345beb06850339d8f8684885976bb207e6
André <andrefm06@gmail.com>
2026-07-03 23:11:40 -0300

    Add Merge Sort.

--- Estudos em Estrutura de Dados/Sorts/src/Algoritimos.java
+++ Estudos em Estrutura de Dados/Sorts/src/Algoritimos.java
@@ -55,4 +55,56 @@ public class Algoritimos {
         }
         System.out.println(Arrays.toString(array));
     }
+    public void mergeSort(){
+
+        System.out.println(Arrays.toString(array));
+        if (array.length > 1) {
+            mergeSortHelper(0, array.length - 1);
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+    private void mergeSortHelper(int head, int tail){
+        if(head < tail){
+            int middle = (head + tail)/2;
+            mergeSortHelper(head, middle);
+            mergeSortHelper(middle + 1, tail);
+            merge(head, middle, tail);
+        }
+    }
+    private void merge(int head, int middle, int tail) {
+        int[] temp = new int[tail - head + 1];
+
+        int leftIndex  = head;
+        int rightIndex = middle + 1;
+        int tempIndex  = 0;
+
+        while (leftIndex <= middle && rightIndex <= tail) {
+
+            if (array[leftIndex] <= array[rightIndex]) {
+                temp[tempIndex] = array[leftIndex];
+                leftIndex++; // Move the left pointer forward
+            }
+            else {
+                temp[tempIndex] = array[rightIndex];
+                rightIndex++; // Move the right pointer forward
+            }
+            tempIndex++;
+        }
+
+        while (leftIndex <= middle) {
+            temp[tempIndex] = array[leftIndex];
+            leftIndex++;
+            tempIndex++;
+        }
+
+        while (rightIndex <= tail) {
+            temp[tempIndex] = array[rightIndex];
+            rightIndex++;
+            tempIndex++;
+        }
+        for (int position = head; position <= tail; position++) {
+            array[position] = temp[position - head];
+        }
+    }
 }
