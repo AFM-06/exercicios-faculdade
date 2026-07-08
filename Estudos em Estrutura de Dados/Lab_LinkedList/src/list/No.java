94324ec87d96555149e67e9387f00000781ff00c
André <andrefm06@gmail.com>
2026-07-08 17:10:39 -0300

    Rename attributes.

--- Estudos em Estrutura de Dados/Lab_LinkedList/src/list/No.java
+++ Estudos em Estrutura de Dados/Lab_LinkedList/src/list/No.java
@@ -1,13 +1,13 @@
 package list;
 
 public class No {
-	Integer dado; //Manter o nome deste atributo
-    No proximo;
-	No anterior;
-	public No(Integer dado, No proximo, No anterior){
-		this.dado = dado;
-		this.proximo = proximo;
-		this.anterior = anterior;
+	Integer data; //Manter o nome deste atributo
+    No next;
+	No previous;
+	public No(Integer data, No next, No previous){
+		this.data = data;
+		this.next = next;
+		this.previous = previous;
 	}
 	public Integer getDado() { //Manter o nome desta função. Ela será utilizada nos testes
 		return dado;
