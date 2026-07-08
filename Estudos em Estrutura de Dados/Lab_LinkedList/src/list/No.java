918c888e76baa2d582e252e81acad0b3c8b60602
André <andrefm06@gmail.com>
2026-07-08 17:05:52 -0300

    Add body to class No.

--- Estudos em Estrutura de Dados/Lab_LinkedList/src/list/No.java
+++ Estudos em Estrutura de Dados/Lab_LinkedList/src/list/No.java
@@ -2,8 +2,13 @@ package list;
 
 public class No {
 	Integer dado; //Manter o nome deste atributo
-    No proxNo;
-
+    No proximo;
+	No anterior;
+	public No(Integer dado, No proximo, No anterior){
+		this.dado = dado;
+		this.proximo = proximo;
+		this.anterior = anterior;
+	}
 	public Integer getDado() { //Manter o nome desta função. Ela será utilizada nos testes
 		return dado;
 	}
