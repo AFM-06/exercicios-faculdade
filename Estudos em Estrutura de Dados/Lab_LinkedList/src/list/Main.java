52791860caa96e3f9ad16f19c785e1db0e4b1e0c
André <andrefm06@gmail.com>
2026-07-09 21:36:03 -0300

    Add tests in class Main.

--- Estudos em Estrutura de Dados/Lab_LinkedList/src/list/Main.java
+++ Estudos em Estrutura de Dados/Lab_LinkedList/src/list/Main.java
@@ -2,6 +2,14 @@ package list;
 
 public class Main {
     public static void main(String[] args) {
-
+        LinkedList lista = new LinkedList();
+        lista.insertFirst(10);
+        lista.insertLast(20);
+        lista.insertFirst(30);
+        lista.print();
+        System.out.println(lista.search(20));
+        lista.removeInicio();
+        System.out.println(lista.size());
+        lista.print();
     }
 }
