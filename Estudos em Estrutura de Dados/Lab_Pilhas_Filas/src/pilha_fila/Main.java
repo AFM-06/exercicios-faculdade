92169a6b1b15c0c0b76c3c82bc3ad2358e3a16d1
André <andrefm06@gmail.com>
2026-07-16 14:38:48 -0300

    runs tests.

--- Estudos em Estrutura de Dados/Lab_Pilhas_Filas/src/pilha_fila/Main.java
+++ Estudos em Estrutura de Dados/Lab_Pilhas_Filas/src/pilha_fila/Main.java
@@ -4,6 +4,15 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-    
+        var queue = new Queue(10);
+        queue.enqueue(16);
+        queue.enqueue(15);
+        queue.dequeue();
+        queue.enqueue(13);
+        queue.enqueue(12);
+        System.out.println(queue.peek());
+        queue.print();
+        Integer[] array = queue.toArray();
+        System.out.println(Arrays.toString(array));
     }
 }
