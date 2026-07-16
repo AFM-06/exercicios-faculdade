ae958343f964ea4226c013d8de513fe93654f7f6
André <andrefm06@gmail.com>
2026-07-15 23:55:31 -0300

    Create 2 different constructors

--- Estudos em Estrutura de Dados/Lab_Pilhas_Filas/src/pilha_fila/QueueNode.java
+++ Estudos em Estrutura de Dados/Lab_Pilhas_Filas/src/pilha_fila/QueueNode.java
@@ -4,3 +4,15 @@ public class QueueNode {
     Integer data;
     QueueNode next;
     QueueNode previous;
+
+    public QueueNode(Integer data) {
+        this.data = data;
+        next = null;
+        previous = null;
+    }
+    public QueueNode() {
+        data = null;
+        next = null;
+        previous = null;
+    }
+}
