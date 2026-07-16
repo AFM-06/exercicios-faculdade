c73582cf06cb8b2047fb172694de489e606c0f77
André <andrefm06@gmail.com>
2026-07-16 20:50:34 -0300

    Adds comments.

--- Estudos em Estrutura de Dados/Lab_Pilhas_Filas/src/pilha_fila/Stack.java
+++ Estudos em Estrutura de Dados/Lab_Pilhas_Filas/src/pilha_fila/Stack.java
@@ -14,7 +14,9 @@ public class Stack implements Stack_IF{
 		size = 0;
 		this.max_size = max_size;
 	}
-	
+
+	// Checks if the stack is empty; otherwise, creates an auxiliary node and checks if the stack contains only one item (top == base). If so,
+	// sets both to null; otherwise, the node preceding the top is set to point to null and is updated to become the new top.
 	@Override
 	public Integer pop() {
 		if(isEmpty()){
@@ -33,7 +35,9 @@ public class Stack implements Stack_IF{
 			return aux.data;
 		}
 	}
-
+	// First, check if the stack is not full; if it isn't, create a node named new_item and check again—but this time,
+	// check if the stack is empty. If it is, the top and the base will represent the same element; otherwise, new_item.previous will point to
+	// the top of the stack, the top of the stack will point back, and finally, new_item will effectively become the top of the stack.
 	@Override
 	public void push(Integer novoElemento) {
 		if(!isFull()){
@@ -56,17 +60,18 @@ public class Stack implements Stack_IF{
 	public boolean isEmpty() {
 		return size == 0;
 	}
-
+	//Just return boolean result of size == max_size, if true Stack is full.
 	@Override
 	public boolean isFull() {
 		return size == max_size;
 	}
-
+	//Return top.data, no less no more.
 	@Override
 	public Integer top() {
 		return top.data;
 	}
-
+	//first, check if the queue is empty; otherwise, create an array of Integers of size [size],
+	//initialize node x at the top, and traverse to the bottom, adding items using index [i].
 	@Override
 	public Integer[] toArray() {
 		if (!isEmpty()) {
@@ -82,7 +87,8 @@ public class Stack implements Stack_IF{
 		}
 		return null;
 	}
-	
+	// Helper method to print the stack. First, it checks if the stack is not empty, then creates an auxiliary node named 'print'
+	// initialized at the top and traverses the entire stack down to the base, printing the node data.
 	public void print(){
 		if(!isEmpty()){
 			var print = top;
