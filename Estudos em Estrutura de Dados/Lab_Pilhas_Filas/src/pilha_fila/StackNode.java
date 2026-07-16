35f366803d89a63a26181e8e733d452d10c76d9c
André <andrefm06@gmail.com>
2026-07-16 19:02:11 -0300

    Create class StackNode and constructors.

--- Estudos em Estrutura de Dados/Lab_Pilhas_Filas/src/pilha_fila/StackNode.java
+++ Estudos em Estrutura de Dados/Lab_Pilhas_Filas/src/pilha_fila/StackNode.java
@@ -1,5 +1,19 @@
 package pilha_fila;
 
 public class StackNode {
-	//TODO
+	Integer data;
+    StackNode next;
+    StackNode previous;
+
+    public StackNode(){
+        data = null;
+        next = null;
+        previous = null;
+    }
+
+    public StackNode(Integer data){
+        this.data = data;
+        next = null;
+        previous = null;
+    }
 }
