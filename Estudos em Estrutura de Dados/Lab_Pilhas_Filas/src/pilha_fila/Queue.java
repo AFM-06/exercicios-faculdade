690de39e8eac0ec8929c359ef695cdfca2fada46
André <andrefm06@gmail.com>
2026-07-16 12:56:41 -0300

    Create toArray method.

--- Estudos em Estrutura de Dados/Lab_Pilhas_Filas/src/pilha_fila/Queue.java
+++ Estudos em Estrutura de Dados/Lab_Pilhas_Filas/src/pilha_fila/Queue.java
@@ -70,7 +70,17 @@ public class Queue implements Queue_IF{
 
 	@Override
 	public Integer[] toArray() {
-		// TODO Auto-generated method stub
+		if (!isEmpty()) {
+			Integer[] array = new Integer[size];
+			var x = head;
+			int i = 0;
+			while (x != null) {
+				array[i] = x.data;
+				i++;
+				x = x.next;
+			}
+			return array;
+		}
 		return null;
 	}
 
