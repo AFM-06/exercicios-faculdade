b7b5417f335e7fd468ead4a276244a2a8f5ae535
André <andrefm06@gmail.com>
2026-08-06 15:08:50 -0300

    Add Node class.

--- Estudos em Estrutura de Dados/Test_DoubleLinkedList/src/TDLL/Node.java
+++ Estudos em Estrutura de Dados/Test_DoubleLinkedList/src/TDLL/Node.java
@@ -1,4 +1,26 @@
 package TDLL;
 
 public class Node {
+    private Node next;
+    private Node previous;
+    private Integer data;
+
+    public Node(){
+        next = null;
+        previous = null;
+        data = null;
+    }
+    
+    public Node(Integer data){
+        this.data = data;
+        next = null;
+        previous = null;
+    }
+
+    public Node getNext() {return next;}
+    public void setNext(Node next) {this.next = next;}
+    public Node getPrevious() {return previous;}
+    public void setPrevious(Node previous) {this.previous = previous;}
+    public Integer getData() {return data;}
+    public void setData(Integer data) {this.data = data;}
 }
