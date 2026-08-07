bb77642ab7c429964d018f8e4d1681a1fbb382bb
André <andrefm06@gmail.com>
2026-08-07 02:16:57 -0300

    change names

--- Estudos em Estrutura de Dados/Test_DoubleLinkedList/src/TDLL/IF_DoubleLinkedList.java
+++ Estudos em Estrutura de Dados/Test_DoubleLinkedList/src/TDLL/IF_DoubleLinkedList.java
@@ -5,11 +5,11 @@ import list.No;
 public interface IF_DoubleLinkedList {
     public boolean isEmpty();
     public int size();
-    public No search(Integer elemento);
+    public Node search(Integer elemento);
     public void insertFirst(Integer novoElemento);
     public void insertLast(Integer novoElemento);
-    public No remove(Integer elemento);
-    public No removeInicio();
-    public No removeFinal();
-    public No[] toArray();
+    public Node remove(Integer elemento);
+    public Node removeInicio();
+    public Node removeFinal();
+    public Node[] toArray();
 }
--- Estudos em Estrutura de Dados/Test_DoubleLinkedList/src/TDLL/Node.java
+++ Estudos em Estrutura de Dados/Test_DoubleLinkedList/src/TDLL/Node.java
@@ -2,25 +2,25 @@ package TDLL;
 
 public class Node {
     private Node next;
-    private Node previous;
+    private Node prev;
     private Integer data;
 
     public Node(){
         next = null;
-        previous = null;
+        prev = null;
         data = null;
     }
-    
+
     public Node(Integer data){
         this.data = data;
         next = null;
-        previous = null;
+        prev = null;
     }
 
     public Node getNext() {return next;}
     public void setNext(Node next) {this.next = next;}
-    public Node getPrevious() {return previous;}
-    public void setPrevious(Node previous) {this.previous = previous;}
+    public Node getPrev() {return prev;}
+    public void setPrev(Node prev) {this.prev = prev;}
     public Integer getData() {return data;}
     public void setData(Integer data) {this.data = data;}
 }
