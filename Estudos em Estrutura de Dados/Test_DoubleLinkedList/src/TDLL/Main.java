e6ac949bf42319f356fc5d48a3ba6b088fe2e9f4
André <andrefm06@gmail.com>
2026-08-07 02:16:29 -0300

    test

--- Estudos em Estrutura de Dados/Test_DoubleLinkedList/src/TDLL/Main.java
+++ Estudos em Estrutura de Dados/Test_DoubleLinkedList/src/TDLL/Main.java
@@ -2,6 +2,11 @@ package TDLL;
 
 public class Main {
     public static void main(String[] args) {
-
+        var dll = new DoubleLinkedList();
+        dll.insertLast(10);
+        dll.insertLast(11);
+        dll.insertLast(12);
+        dll.insertFirst(9);
+        dll.print();
     }
 }
