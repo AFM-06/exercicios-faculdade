2c81bcaf599607697fdd41b24e5408b20772b656
André <andrefm06@gmail.com>
2026-08-15 18:35:53 -0300

    Add Constructors and toString.

--- Estudos em Estrutura de Dados/Prova_Unidade_02/DoubleLinkedList/Node.java
+++ Estudos em Estrutura de Dados/Prova_Unidade_02/DoubleLinkedList/Node.java
@@ -6,6 +6,22 @@ public class Node {
     Node previous;
 
     public Node(){
-
+        data = null;
+        next = null;
+        previous = null;
+    }
+    public Node(Integer data){
+        this.data = data;
+        next = null;
+        previous = null;
+    }
+    public Node(Integer data, Node next, Node previous){
+        this.data = data;
+        this.next = next;
+        this.previous = previous;
+    }
+    @Override
+    public String toString(){
+        return ""+data;
     }
 }
