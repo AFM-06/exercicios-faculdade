131812edabeab4b8a373f0f45bd946572a11d042
André <andrefm06@gmail.com>
2026-08-15 18:35:31 -0300

    Implements Interface

--- Estudos em Estrutura de Dados/Prova_Unidade_02/DoubleLinkedList/DoubleLinkedList.java
+++ Estudos em Estrutura de Dados/Prova_Unidade_02/DoubleLinkedList/DoubleLinkedList.java
@@ -1,4 +1,79 @@
 package DoubleLinkedList;
 
-public class DoubleLinkedList {
+public class DoubleLinkedList implements DoubleLinkedList_Interface {
+    private Node head;
+    private Node tail;
+    private Integer size;
+
+    public DoubleLinkedList(){
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+
+    @Override
+    public boolean isEmpty() {
+        return size==0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Node search(Integer elemento) {
+        return null;
+    }
+
+    @Override
+    public void insertFirst(Integer novoElemento) {
+
+    }
+
+    @Override
+    public void insertLast(Integer novoElemento) {
+
+    }
+
+    @Override
+    public Node remove(Integer elemento) {
+        return null;
+    }
+
+    @Override
+    public Node removeInicio() {
+        return null;
+    }
+
+    @Override
+    public Node removeFinal() {
+        return null;
+    }
+
+    @Override
+    public Node[] toArray() {
+        return new Node[0];
+    }
+
+    @Override
+    public Node getMinimum() {
+        return null;
+    }
+
+    @Override
+    public Node getMax() {
+        return null;
+    }
+
+    @Override
+    public Node[] toArrayInverse() {
+        return new Node[0];
+    }
+
+    @Override
+    public void print() {
+
+    }
 }
